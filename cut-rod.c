e588e13010b0157a139e96bddf7fd5ec5ae9e76f
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2024-02-13 19:12:00 -0300

    printing optimal solution

--- cut-rod.c
+++ cut-rod.c
@@ -1,12 +1,14 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int cut_rod(int *p, int n) {
   if (n == 0) {
     return 0;
   }
-  int q = -INFINITY;
+  int q = -1;
   for (int i = 1; i <= n; i++) {
     q = fmaxf(q, p[i] + cut_rod(p, n - i));
   }
@@ -57,10 +59,63 @@ int bottom_up_cut_rod(int *p, int n) {
   return r[n];
 }
 
-int main() {
-  int prices[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-  int n = 10;
+int *extended_cut_rod(int *p, int n, int *s) {
+  int *r = calloc(n + 1, sizeof(int));
+  memset(r, -1, sizeof(*r));
+  r[0] = 0;
 
-  printf("Max revenue for 30: %d\n", memoized_cut_rod(prices, 300));
-  printf("Max revenue for 30: %d\n", bottom_up_cut_rod(prices, 300));
+  for (int j = 1; j <= n; j++) {
+    int q = -1;
+    for (int i = 1; i <= j; i++) {
+      if (q < p[i] + r[j - i]) {
+        q = p[i] + r[j - i];
+        s[j] = i;
+      }
+    }
+    r[j] = q;
+  }
+
+  return r;
+}
+
+void print_cut_rod_solution(int *p, int n) {
+  int s[n + 1];
+  memset(s, -1, sizeof(s));
+
+  int *r = extended_cut_rod(p, n, s);
+
+  printf("Optimal solution for %d: ", n);
+  while (n > 0) {
+    printf("%d ", s[n]);
+    n -= s[n];
+  }
+
+  printf("\n");
+}
+
+void gen_prices(int *ps, int n) {
+  ps[0] = 0;
+  for (int i = 1; i < n; i++) {
+    ps[i] = (int)(10.0 * rand() / (RAND_MAX + 1.0));
+  }
+}
+
+void print_prices(int *ps, int n) {
+  printf("prices: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", ps[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int n = 8;
+  int prices[n];
+  srand(time(NULL));
+  gen_prices(prices, n);
+  print_prices(prices, n);
+
+  printf("Max revenue for %d: %d\n", n, memoized_cut_rod(prices, n));
+  printf("Max revenue for %d: %d\n", n, bottom_up_cut_rod(prices, n));
+  print_cut_rod_solution(prices, n);
 }
